14ca9a437552d859ae7cbeba3a0e2db3aead6611
Ankit Singh <ankitsingh0702@gmail.com>
2022-03-13 21:21:11 +0530

    bug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,17 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-   GoogleProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
   pages:{
-      signIn: "/auth/signin"
+      signIn:'/auth/signin'
   },
   callbacks:{
     async session({session, token, user}){
